2579866d1bd006b6e5ac83885a9092c171b38ac5
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-06-29 10:01:21 -0400

    prompt functionality working; need to handle license badge; need to split function up to follow assignment template

---
+++ README.md
@@ -0,0 +1,33 @@
+# Test 1
+
+## Description
+Test 2
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Test 3
+
+## Usage
+Test 4
+
+## License
+BSD 3-Clause New or Revised License
+
+## Contributing
+Test 5
+
+## Tests
+Test 6
+
+## Questions
+You can see my github repositories at Test 7.
+Drop me a line at Test 8.
+
+## Badges
--- index.js
+++ index.js
@@ -18,14 +18,15 @@ const inquirer = require ("inquirer");
  
 //below is the boilerplate text for the README.md file with variables for incoming string data
 //I believe badges go at the top of the file, not in a separate section, but I've left it there for now
-const writeToFile = ({title, description, installation, usage, license, contributing, tests, github, email}) =>
+//add links to the sections
+const newReadMe = ({title, description, installation, usage, license, contributing, tests, github, email}) =>
 `# ${title}
 
 ## Description
 ${description}
 
 ## Table of Contents
-- [Installation](#installation) //links to the sections
+- [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
 - [Contributing](#contributing)
@@ -53,69 +54,70 @@ Drop me a line at ${email}.
 
 ## Badges`;
 
+inquirer
+    .prompt ([
+        {
+            name: "title", 
+            message: questions[0],
+            type: "input"
+        },
+        {
+            name: "description",
+            message: questions[1],
+            type: "input"
+        },
+        {
+            name: "installation",
+            message: questions[2],
+            type: "input"
+        },
+        {
+            name: "usage",
+            message: questions[3],
+            type: "input"
+        },
+        {
+            name: "license",
+            message: questions[4],
+            type: "list",
+            choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause Simplified License", "BSD 3-Clause New or Revised License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License v2.0", "GNU Lesser General Public License v2.0", "Mozilla Public License 2.0", "The Unilicense"]
+        },
+        {
+            name: "contributing",
+            message: questions[5],
+            type: "input"
+        },
+        {
+            name: "tests",
+            message: questions[6],
+            type: "input"
+        },
+        {
+            name: "github",
+            message: questions[7],
+            type: "input"
+        },
+        {
+            name: "email",
+            message: questions[8],
+            type: "input"
+        }
+    ])
+    .then ((responses) => {
+        const myData = newReadMe(responses)
+        fs.writeFile("README.md", `${myData}`, (err) =>
+            err ? console.log(err) : console.log("Your README.md file is complete.") 
+        )
+    })
+    
+
+
 // TODO: Create a function to write README file
 function writeToFile (fileName, data) {
  }
 
 // TODO: Create a function to initialize app
 function init () {
-    inquirer
-        .prompt ([
-            {
-                name: "title", 
-                message: questions[0],
-                type: "input"
-            }
-            {
-                name: "description",
-                message: questions[1],
-                type: "input"
-            }
-            {
-                name: "installation",
-                message: questions[2],
-                type: "input"
-            }
-            {
-                name: "usage",
-                message: questions[3],
-                type: "input"
-            }
-            {
-                name: "license",
-                message: questions[4],
-                type: "list",
-                choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause Simplified License", "BSD 3-Clause New or Revised License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License v2.0", "GNU Lesser General Public License v2.0", "Mozilla Public License 2.0", "The Unilicense"]
-            }
-            {
-                name: "contributing",
-                message: questions[5],
-                type: "input"
-            }
-            {
-                name: "tests",
-                message: questions[6],
-                type: "input"
-            }
-            {
-                name: "github",
-                message: questions[7],
-                type: "input"
-            }
-            {
-                name: "email",
-                message: questions[8],
-                type: "input"
-            }
-        ])
-        .then ((responses) => {
-                const myData = writeToFile(responses)
-                fs.writeFile("README.md", `${myData}`, (err) =>
-                    err ? console.log(err) : console.log("Your README.md file is complete.") 
-                )
-            })
-        }
-            // console.log(question, questions[question])
     }
 
 // Function call to initialize app
